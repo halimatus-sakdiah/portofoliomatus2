e4a0a968372c3d5f2043c2ac0e15846431cf19da
halimatus-sakdiah <matushalimatus2606@gmail.com>
2023-12-12 00:10:44 +0700

    first commit

---
+++ database/migrations/2023_12_11_144441_create_cruds_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cruds', function (Blueprint $table) {
+            $table->id();
+            $table->char('idnumber', 7);
+            $table->string('fullname', 100);
+            $table->enum('gender', ['M', 'F']);
+            $table->string('address', 100);
+            $table->char('phone', 20);
+            $table->string('emailaddres', 100);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cruds');
+    }
+};
